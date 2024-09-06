72194fab6e977e5c3d834f0aa397d7fcda354036
techanalyst84 <s.suneeth@gmail.com>
2024-09-06 16:41:44 -0400

    logs and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,10 +15,15 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        
     
     def __str__(self):
         return self.error_message
     
 
-
-        
+#if __name__=="__main__":
+#    try:
+#        a=1/0
+#     except Exception as e:
+#        logging.info("Divind by zero")
+#        raise CustomException(e,sys)
